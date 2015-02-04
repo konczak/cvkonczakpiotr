d07261f0f96048ed07fa9bd2ab4670fab69c40c3
Piotr Konczak <piotr.konczak@cybercom.com>
2015-02-04 11:54:57 +0100

    generate app with yo angular without Compass with Bootstrap

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name konczakpiotrcvApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the konczakpiotrcvApp
+ */
+angular.module('konczakpiotrcvApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
