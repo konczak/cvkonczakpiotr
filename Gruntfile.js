4b1d88d4a1266fad2f5438841e630739bebe0a17
Piotr Konczak <konczak.piotrek@gmail.com>
2015-03-01 16:22:35 +0100

    add grunt-build-control plugin and configuration to make it possible to deploy automatically dist into production (branch prod)

--- Gruntfile.js
+++ Gruntfile.js
@@ -371,7 +371,22 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
-    }
+    },
+	
+	buildcontrol: {
+		options: {
+		  dir: 'dist',
+		  commit: true,
+		  push: true,
+		  message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+		},
+		pages: {
+		  options: {
+			remote: 'git@github.com:konczak/konczak.github.io.git',
+			branch: 'master'
+		  }
+		}
+	  }
   });
 
 
